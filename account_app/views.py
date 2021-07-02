d02b2fccda701f1bf2172f1e3e7efcd0019ed003
CiceronRiman <43729961+CiceronRiman@users.noreply.github.com>
2021-07-02 21:11:15 +0300

    Update views.py

--- account_app/views.py
+++ account_app/views.py
@@ -8,8 +8,9 @@ class RegisterView(APIView):
     def post(self, request):
         serializer = RegisterAccountSerializer(data=request.data)
         data = {}
-        if serializer.is_valid():
+        if serializer.is_valid():  # тут можно добавить is_valid(raise_exception=True), если не пройдет валидацию будет вызвано исключение и оно обернется в http ответ
             account = serializer.save()
+            # Вместо трех строк снизу, стоит создать схему сериалазитора для ответа
             data['respoinse'] = 'succsesful registred account.'
             data['email'] = account.email
             data['username'] = account.username
